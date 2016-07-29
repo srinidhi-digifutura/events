4d3c89171c323431b606c810192730344999efc2
Srinidhi Gs <srinidhi@digifutura.com>
2016-07-29 23:50:55 +0530

    attending table for a user and a event

---
+++ db/migrate/20160729181949_create_assemblies.rb
@@ -0,0 +1,10 @@
+class CreateAssemblies < ActiveRecord::Migration
+  def change
+    create_table :assemblies do |t|
+      t.integer :user_id
+      t.integer :event_id
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729174342) do
+ActiveRecord::Schema.define(version: 20160729181949) do
+
+  create_table "assemblies", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
