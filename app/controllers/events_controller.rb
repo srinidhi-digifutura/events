cdfab3379af8ebd7677e6b14c6596a6ce7cdfa12
Srinidhi Gs <srinidhi@digifutura.com>
2016-07-29 21:55:07 +0530

    creating new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,23 @@
 class EventsController < ApplicationController
 
 	def index
+        @events = Event.all
 	end
 
 	def new
+        @event = Event.new
 	end
 
     def show
     end
 
     def create
+        @event = Event.new(event_params)
+        if @event.save
+            redirect_to events_path
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -21,4 +29,9 @@ class EventsController < ApplicationController
     def destroy
     end
 
+    private
+
+    def event_params
+        params.require(:event).permit(:title,:description,:event_date,:venue,:address,:state,:city)
+    end
 end
