e91b024f1ea3a0860174d5d35d479a020c3fb50d
Srinidhi Gs <srinidhi@digifutura.com>
2016-07-31 00:37:14 +0530

    added gender to users and cdd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730141045) do
+ActiveRecord::Schema.define(version: 20160730183247) do
 
   create_table "assemblies", force: :cascade do |t|
     t.integer  "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160730141045) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
