dcfe20c29a20304306c7fd58bda03dac4da80bf4
Srinidhi Gs <srinidhi@digifutura.com>
2016-08-01 12:04:55 +0530

    misc changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ActiveRecord::Base
 	has_many :assemblies
 	validates :title,:venue, :city, :state, :event_date, :ticket_fee,presence: true
 	validates :address,:description,length: {minimum: 10}
+	validates :ticket_fee,numericality:{integer:true}
 end
